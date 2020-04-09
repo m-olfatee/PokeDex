86d4e0fffa41cfd3d1dce21d968cda717b4dd83a
m-olfatee <m.olfatee@gmail.com>
2020-04-09 08:51:35 +0200

    css finished

--- src/Components/PokeItem.js
+++ src/Components/PokeItem.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './pokeitem.css';
+import PokePictures from "../Components/PokePictures"
 
 const PokeItem = (props) => {
+    for (let i = 0; i < PokePictures.length; i++) {
+
+        if (i == props.number) {
+            var image = PokePictures[i].src
+            console.log(image)
+        }
+    }
+
     return (
-
-        <figure>
-            <img src='https://pokeres.bastionbot.org/images/pokemon/25.png' alt="" />
+        <figure className="pokemon">
+            <img className="img-container" src={image} alt="" />
             <figcaption>
-                <h3>Title:</h3>
-
-                <a href='#'>Check out</a>
+                <h3 className="name">{props.name}</h3>
+                <a className="link" href={props.url}>Check out</a>
             </figcaption>
         </figure>
     )
--- src/Components/PokeList.js
+++ src/Components/PokeList.js
@@ -3,30 +3,31 @@ import PokeItem from './PokeItem';
 import './pokelist.css';
 
 class PokeList extends Component {
-    state = { PokeDex: [] }
+    state = {
+        PokeDex: []
+    }
 
     componentDidMount = () => {
-        fetch('')
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
-                this.setState({ PokeDex: data })
+                this.setState({ PokeDex: data.results })
             });
     }
+
     render() {
         return (
             <section>
-                <PokeItem />
-                <PokeItem />
-                <PokeItem />
-                <PokeItem />
-                <PokeItem />
-                <PokeItem />
-                <PokeItem />
-                    )
-                }
-                )}
+                <h1>PokeDex</h1>
+                <div className="poke-container" >
+                    {this.state.PokeDex.map((pokemon, i) => {
+                        return (<PokeItem name={pokemon.name} url={pokemon.url} key={i} number={i}
+                        />)
+                    })}
+                </div>
+                <button className="sort-button">Sort A to Z</button>
             </section>);
     }
 }
